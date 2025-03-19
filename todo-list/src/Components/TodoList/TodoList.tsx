6b2dfc07336687e06a61efd03769e279c2974d82
Manh Thang <thangnmjob.com>
2025-03-20 00:00:44 +0700

    [ADD] : Add local store

--- todo-list/src/Components/TodoList/TodoList.tsx
+++ todo-list/src/Components/TodoList/TodoList.tsx
@@ -114,7 +114,7 @@ import React from 'react'
 import { useReducer, useState } from 'react'
 // import reducer_Todo from '../UseReducer/UseReducer'
 import { Input, Button, Checkbox } from 'antd'
-import { CloseOutlined, CarryOutOutlined} from '@ant-design/icons'
+import { CloseOutlined, CarryOutOutlined } from '@ant-design/icons'
 import "tailwindcss";
 import Todo from '../Todos/Todo';
 
@@ -128,27 +128,38 @@ type Action = | { type: "ADD_todo"; payload: string } | { type: "TOGGLE_todo"; p
 
 
 // mang ban dau trong
-const InitialState: Todo[] = [];
+
+const time : Date = new Date()
 
 function reducer_Todo(state: Todo[], action: Action): Todo[] {
+
+    let newState;
+
     switch (action.type) {
-        case "ADD_todo":{
-            return  [...state, { id: Date.now(), content: action.payload, complete: false }];
-            
+        case "ADD_todo": {
+            newState = [...state, { id: Date.now(), content: action.payload, complete: false }];
+
         }
-            
+        break;
+
         case "TOGGLE_todo":
-            return state.map(todo =>
+            newState = state.map(todo =>
                 todo.id === action.payload ? { ...todo, complete: !todo.complete } : todo
             );
+
+        break;
         case "DELE_todo":
-            return state.filter(todo => todo.id !== action.payload);
+            newState = state.filter(todo => todo.id !== action.payload);
+        break
         default:
             return state;
     }
+    localStorage.setItem("todos", JSON.stringify(newState));
+    return newState;
 }
 
-
+const InitialState: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]");
+//const initialState: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]");
 
 function TodoList() {
     const [state, dispatch] = useReducer(reducer_Todo, InitialState);
@@ -160,39 +171,54 @@ function TodoList() {
             setText("");
         }
     }
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter" && text.trim() !== "") {
+            dispatch({ type: "ADD_todo", payload: text });
+            setText(""); // Xóa nội dung input sau khi thêm
+            focus()
+        }
+    };
+
+
     return (
         <div className="container">
             <div className="content">
                 <h1 className="title"> <CarryOutOutlined /> Todo List</h1>
                 <div className="input_content">
-                    <Input  
+                    <Input
                         className="input"
                         type="text"
                         value={text}
                         onChange={e => setText(e.target.value)}
-                        placeholder="Thêm công việc...">
-                    
+                        placeholder="Thêm công việc..."
+                        onKeyDown={handleKeyDown}
+                        >
+                        
                     </Input>
                     <Button
-                        className='button' 
-                        onClick={handleAddTodo}>
+                        className='button'
+                        onClick={handleAddTodo}
+                    >
                         Add
                     </Button>
                 </div>
                 <ul className='list'>
                     {state.map(todo => (
                         <li key={todo.id} className="" >
-                                <Checkbox className='check_box'
-                                
-                                    onChange={() => dispatch({ type: "TOGGLE_todo", payload: todo.id })}
-                                    checked={todo.complete}                                    
-                                >
-                                </Checkbox>
-                                {todo.content}
+                            <Checkbox className='check_box'
+
+                                onChange={() => dispatch({ type: "TOGGLE_todo", payload: todo.id })}
+                                checked={todo.complete}
+                            >
+                            </Checkbox>
+                            {todo.content} 
+
+                            {/* [{time.getHours()}:{time.getMinutes()}] [{time.getDate()}/{time.getUTCMonth() + 1}/{time.getFullYear()}] */}
                             <Button onClick={() => dispatch({ type: "DELE_todo", payload: todo.id })}
                                 className=""
                             >
-                            <CloseOutlined />
+                                <CloseOutlined />
                             </Button>
                         </li>
                     ))}
